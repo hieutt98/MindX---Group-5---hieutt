ab09cf2c9062ee20f01fe20ea247985678b8844e
hieutt98 <trantrunghieu@gmail.com>
2022-01-08 23:27:22 +0700

    Lesson 8 - 1b - Calculator

---
+++ Lesson 8 - Ex1b - Group 5 - hieutt - 08012022/Ex1b.js
@@ -0,0 +1,23 @@
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+let num1 = parseInt(prompt("Enter number 1: "));
+let num2 = parseInt(prompt("Enter number 2: "));
+let result;
+switch (operator) {
+    case "+": 
+        result = num1 + num2;
+        break;
+
+    case "-": 
+        result = num1 - num2;
+        break;
+
+    case "*": 
+        result = num1 * num2;
+        break;
+
+    case "/": 
+        result = parseFloat(num1 / num2);
+        break;
+};
+document.getElementById("result-view").innerHTML = `Phép tính của bạn là: ${num1} ${operator} ${num2} = ${result}`;
+console.log(`${a} ${operator} ${b} = ${result}`);
