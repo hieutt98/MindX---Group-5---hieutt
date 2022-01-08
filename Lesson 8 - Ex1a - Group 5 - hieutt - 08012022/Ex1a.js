a91f7838423b93ae3cbec1ec8e78631fdad8a00b
hieutt98 <trantrunghieu@gmail.com>
2022-01-08 23:14:13 +0700

    Exercise 8 - 1a - Calculator

---
+++ Lesson 8 - Ex1a - Group 5 - hieutt - 08012022/Ex1a.js
@@ -0,0 +1,16 @@
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+let result;
+let num1 = parseInt(prompt("Enter number 1: "));
+let num2 = parseInt(prompt("Enter number 2: "));
+if (operator === "+") {
+    result = num1 + num2;
+} else if (operator === "-") {
+    result = num1 - num2;
+} else if (operator === "*") {
+    result = num1 * num2;
+} else if (operator === "/") {
+    result = num1 / num2;
+}
+
+document.getElementById("result-view").innerHTML = `Phép tính của bạn là: ${num1} ${operator} ${num2} = ${result}`;
+console.log(`${a} ${operator} ${b} = ${result}`);
